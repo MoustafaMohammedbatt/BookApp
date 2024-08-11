47a969e85e912395e7d7de7211910ca0f665decd
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-08-11 15:34:23 +0200

    AddReposatories

---
+++ Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entites;
+using Persistence.Data;
+using Service.Abstractions.Interfaces.IBaseRepository;
+using Service.Abstractions.Interfaces.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IBaseRepository<AppUser> ApplicationUsers => new BaseRepository<AppUser>(_context);
+        public IBaseRepository<Book> Books => new BaseRepository<Book>(_context);
+
+        public IBaseRepository<Category> Categories => new BaseRepository<Category>(_context);
+
+        public IBaseRepository<Sold> Solds => new BaseRepository<Sold>(_context);
+
+        public IBaseRepository<Rented> Renteds => new BaseRepository<Rented>(_context);
+
+        public IBaseRepository<Author> Authors => new BaseRepository<Author>(_context);
+
+        public IBaseRepository<Cart> Carts => new BaseRepository<Cart>(_context);
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
---
+++ Service.Abstractions/Interfaces/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entites;
+using Service.Abstractions.Interfaces.IBaseRepository;
+
+namespace Service.Abstractions.Interfaces.IRepositories
+{
+    public interface IUnitOfWork
+    {
+        IBaseRepository<AppUser> ApplicationUsers { get; }
+        IBaseRepository<Category> Categories { get; }
+        IBaseRepository<Book> Books { get; }
+        IBaseRepository<Sold> Solds { get; }
+        IBaseRepository<Rented> Renteds { get; }
+        IBaseRepository<Author> Authors { get; }
+        IBaseRepository<Cart> Carts { get; }
+
+        int Complete();
+    }
+}
