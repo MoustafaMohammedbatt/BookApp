14b292fd868bb405c369554a8e3521510af84e29
Moustafa Batt <114432989+MoustafaMohammedbatt@users.noreply.github.com>
2024-08-16 17:04:11 +0200

    add controller to rented

---
+++ BookApp/Controllers/RentedsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Service.Abstractions.Interfaces.IRepositories;
+
+namespace BookApp.Controllers
+{
+    public class RentedsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RentedsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: /Cart
+        public async Task<IActionResult> Index()
+        {
+            var carts = await _unitOfWork.Renteds.FindAll(c => c.Id > 0, include: q => q.Include(c => c.Book).Include(c=>c.User));
+            return View(carts);
+        }
+
+        
+    }
+}
