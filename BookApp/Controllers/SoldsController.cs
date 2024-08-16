fc8245a60b726aa4df39bf33157534ffb0daa8da
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-08-16 17:24:34 +0200

    AddSoldTableData

---
+++ BookApp/Controllers/SoldsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Service.Abstractions.Interfaces.IRepositories;
+
+namespace BookApp.Controllers
+{
+    public class SoldsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SoldsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: /Cart
+        public async Task<IActionResult> Index()
+        {
+            var carts = await _unitOfWork.Solds.FindAll(c => c.Id > 0, include: q => q.Include(c => c.Book).Include(c => c.User));
+            return View(carts);
+        }
+
+    }
+}
