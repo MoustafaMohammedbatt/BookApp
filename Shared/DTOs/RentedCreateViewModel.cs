c4c4f0362a0d5bf9cd6bae7538d0de1e4973fa23
Moustafa Batt <114432989+MoustafaMohammedbatt@users.noreply.github.com>
2024-08-19 08:54:13 +0200

    edit sold

---
+++ Shared/DTOs/RentedCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class RentedCreateViewModel
+    {
+        public List<BookDTO> Books { get; set; } = new List<BookDTO>();
+        public int CartId { get; set; }
+        public string UserId { get; set; }
+    }
+}
